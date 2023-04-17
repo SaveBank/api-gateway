fcb25dc197ad13c7751b988e4e133726906a3feb
Lwazi <lwazigxogxa@gmail.com>
2023-04-17 21:58:25 +0200

    first commit

---
+++ src/main/java/SaveBankApiGateway/SaveBankApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package SaveBankApiGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaveBankApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaveBankApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SaveBankApiGateway/SaveBankApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package SaveBankApiGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaveBankApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
